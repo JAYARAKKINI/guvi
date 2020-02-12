d4b196d5c1c0050fbc7e5f01e08bd61ef423e741
JAYARAKKINI <37235960+JAYARAKKINI@users.noreply.github.com>
2020-02-12 23:11:25 +0530

    Create marklist.py

---
+++ marklist.py
@@ -0,0 +1,13 @@
+#Grade for marks                                                                                                                                                                                                                                                                                        
+
+mark=int(input('Enter your marks:'))  #Enter the mark to find grade
+if(mark>=90):
+  print('S Grade')
+elif(mark>=80):
+  print('A Grade')
+elif(mark>=60):
+  print('B Grade')
+elif(mark>=40):
+  print('C Grade')
+else:
+  print('FAIL')  

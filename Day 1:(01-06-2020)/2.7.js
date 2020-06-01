9e46b15ea3ea3bb2a18402ae112b89192dc2c342
JAYARAKKINI <37235960+JAYARAKKINI@users.noreply.github.com>
2020-06-02 01:48:08 +0530

    Create 2.7.js

---
+++ Day 1:(01-06-2020)/2.7.js
@@ -0,0 +1,39 @@
+//  Print the age of Nodejs author in console.
+
+
+function college()  // creating function named college
+	{
+		var library=[    //create a variable library
+		{
+			title:"Javascript",
+			price:500,
+			readers:[
+            {
+                name:"Person 1",
+                count:300
+            },
+            {
+                name:"Person 2",
+                count:400
+            }
+            ],
+            authorDetails:{
+                name:"Raj",
+                age:40
+            },
+        },
+		{
+			title:"Node js",
+			price:600,
+			readers:[],
+			authorDetails:{
+                name:"raj",
+                age:40
+        }
+    }
+ 
+]
+	console.log(library[1].authorDetails.age);    //Printing the age of nodejs author 
+	
+}
+college();    // Call function college

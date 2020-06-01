ffb31a63c668bda5960babc6ca552b31206e0808
JAYARAKKINI <37235960+JAYARAKKINI@users.noreply.github.com>
2020-06-02 01:25:57 +0530

    Create 2.2.js

---
+++ Day 1:(01-06-2020)/2.2.js
@@ -0,0 +1,40 @@
+// Insert a new key called email and assign a value email1@gmail.com and email2@gmail.com respectively in both authorDetails object;
+
+function college()  // creating function named college
+	{
+		var library=[    //create a variable library
+		{
+			title:"Javascript",
+			price:500,
+			readers:[
+            {
+                name:"Person 1",
+                count:300
+            },
+            {
+                name:"Person 2",
+                count:400
+            }
+            ],
+            authorDetails:{
+                name:"Raj",
+                age:40
+            },
+        },
+		{
+			title:"Node js",
+			price:600,
+			readers:[],
+			authorDetails:{
+                name:"raj",
+                age:40
+        }
+    }
+ 
+]
+	library[0].authorDetails.emai1="email1@gmail.com";	//Inserting a new key called email in authorDetails
+    console.log(library[0].authorDetails.emai1);
+	library[1].authorDetails.email="email2@gmail.com";  //Inserting a new key called email in authorDetails
+	console.log(library[1].authorDetails.email)
+}
+college();    // Call function college
